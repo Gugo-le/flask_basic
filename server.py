aa88ed479ad3c4d5d537a54051aa5151990844c3
Gugo-le <rngustmd0719@naver.com>
2023-08-31 15:33:05 +0900

    random

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hi'
+    return str(random.random())
 
 app.run(port = 5000, debug=True)
